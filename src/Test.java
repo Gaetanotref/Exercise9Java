d76a9a58d6693d6fc1ca61661c3d35e3ed8e9188
Gaetano Trefiletti <gaetano.trefiletti.gt@gmail.com>
2022-12-06 17:09:58 +0100

    Commit init

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        //CONSTRUCTOR METHOD
+        Animal animal1 = new Animal("Lion");
+        //OVERLOAD METHOD
+        animal1.animalSound();
+        animal1.animalSound(12);
+    }
+}
